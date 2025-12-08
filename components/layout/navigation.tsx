e362324cba4fc95cefb8570ed59c0a813d5430c0
okshanaby <steamnature006@gmail.com>
2025-12-08 18:50:08 +0300

    Implement role-based navigation with admin access in the UI

--- components/layout/navigation.tsx
+++ components/layout/navigation.tsx
@@ -1,17 +1,39 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 import {
   SignInButton,
   SignOutButton,
   SignUpButton,
   SignedIn,
   SignedOut,
+  useUser,
 } from "@clerk/nextjs";
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export const Navigation = () => {
+  const user = useUser();
+
+  const isAdmin = user.user?.publicMetadata?.role === "admin";
+
   return (
     <nav className="border-b border-[var(--foreground)]/10">
       <div className="flex container h-16 items-center justify-between px-4  mx-auto">
         <div className="text-xl font-semibold">RAG Chatbot</div>
+
+        <div className="flex gap-2">
+          <Button variant="ghost">
+            <Link href="/">Home</Link>
+          </Button>
+          {isAdmin && (
+            <Button variant="ghost">
+              <Link href="/upload">Upload</Link>
+            </Button>
+          )}
+          <Button variant="ghost">
+            <Link href="/chat">Chat</Link>
+          </Button>
+        </div>
 
         <div className="flex gap-2">
           <SignedOut>
